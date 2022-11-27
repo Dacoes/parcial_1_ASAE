21bb36fe1a4b711020dc10522787352a0a216bde
David Camilo <davideescobarr@gmail.com>
2022-11-26 19:25:56 -0500

    start proyect

---
+++ parcial/src/main/java/co/unicauca/parcial/model/Persona.java
@@ -0,0 +1,11 @@
+package co.unicauca.parcial.model;
+
+
+public class Persona {
+    private Integer idPersona;
+    private String noIdentificacion;
+    private String TipoIdentificacion;
+    private String nombres;
+    private String apellidos;
+
+}
